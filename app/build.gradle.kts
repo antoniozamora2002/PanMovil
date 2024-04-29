e0366ff49bd047a27784d1c480dfbcb2dc8f44f6
antoniozamora2002 <antonio.zamora@upeu.edu.pe>
2024-04-29 09:16:51 -0500

    testing de app en gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("org.sonarqube") version "5.0.0.4638"
 }
 
 android {
@@ -33,6 +34,15 @@ android {
         // Puedes agregar más exclusiones si es necesario
     }
 
+}
+sonarqube {
+    properties {
+        property("sonar.projectKey", "Pan-Movil-Testing")
+        property("sonar.host.url", "http://localhost:9000")
+        property("sonar.token", "sqp_ee7eb962172e00058b144103bdb228e31a2134f8")
+        property("sonar.login", "admin")
+        property("sonar.password", "admin1")
+    }
 }
 
 dependencies {
