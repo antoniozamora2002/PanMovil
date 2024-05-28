cbf4b8ac3edcfd232e2836270d4639c2d147ba1e
Marco Rios <marco.rios@upeu.edu.pe>
2024-05-28 10:05:27 -0500

    Archivo jenkinsfile.txt añadido y build.gradle.kts configurado para sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,18 @@ plugins {
     id("org.sonarqube") version "5.0.0.4638"
 }
 
-sonarqube {
-    properties {
-        property("sonar.projectKey", "Pan-Movil-Testing")
-        property("sonar.host.url", "http://localhost:9000")
-        property("sonar.token", "sqp_ee7eb962172e00058b144103bdb228e31a2134f8")
-        property("sonar.login", "admin")
-        property("sonar.password", "admin1")
-    }
+//sonarqube {
+  //  properties {
+    //    property("sonar.projectKey", "Pan-Movil-Testing")
+      //  property("sonar.host.url", "http://localhost:9000")
+        //property("sonar.token", "sqp_ee7eb962172e00058b144103bdb228e31a2134f8")
+        //property("sonar.login", "admin")
+        //property("sonar.password", "admin1")
+    //}
+    //}
+sonar {
+  properties {
+    property "sonar.projectKey", "PanMovil"
+    property "sonar.projectName", "PanMovil"
+  }
 }
