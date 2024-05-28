0a41f7f7b4ed21ccd1e9c1f6c552c295966bc314
Marco Rios <marco.rios@upeu.edu.pe>
2024-05-28 10:14:21 -0500

    build.gradle.kts arreglado3

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,16 +3,6 @@ plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.sonarqube") version "5.0.0.4638"
 }
-
-//sonarqube {
-  //  properties {
-    //    property("sonar.projectKey", "Pan-Movil-Testing")
-      //  property("sonar.host.url", "http://localhost:9000")
-        //property("sonar.token", "sqp_ee7eb962172e00058b144103bdb228e31a2134f8")
-        //property("sonar.login", "admin")
-        //property("sonar.password", "admin1")
-    //}
-    //}
 sonar {
   properties {
     property("sonar.projectKey", "PanMovil")
